350bce1c8c0668017709355315b622fcea546d3f
Elvio Batista <elvio.batista@pebmed.com.br>
2022-09-29 18:19:57 -0300

    feat: garante que Signup Controller usa a interface correta

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,20 @@
+import { HttpRequest, HttpResponse } from './../protocols/http'
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
-    }
-
-    if (!httpRequest.body.email) {
+    } else if (!httpRequest.body.email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
+      }
+    } else {
+      return {
+        statusCode: 500,
+        body: {}
       }
     }
   }

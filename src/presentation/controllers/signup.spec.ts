34b9c00cdaac962e3f5e52ce8460c6537eb7c8d3
Elvio Batista <elvio.batista@pebmed.com.br>
2022-09-30 19:04:59 -0300

    test: adiciona teste para verificar se o emailvalidator é chamado com o e-mail correto

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -99,4 +99,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Deve chamar o EmailValidator com o e-mail correto', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'name',
+        email: 'email_inserido@mail.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
+  })
 })

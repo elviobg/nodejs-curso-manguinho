e101b380acc95349761cfcda1cb1edd587ff6fa2
Elvio Batista <elvio.batista@pebmed.com.br>
2022-09-30 17:59:06 -0300

    refactor: redução de código repetido

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,15 +3,16 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    } else if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    } else {
-      return {
-        statusCode: 500,
-        body: {}
+    const requiredFileds = ['name', 'email']
+
+    for (const field of requiredFileds) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
+    }
+    return {
+      statusCode: 500,
+      body: {}
     }
   }
 }

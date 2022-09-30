df1aa6b4672552311682f0e7518b7b0a9b9a1589
Elvio Batista <elvio.batista@pebmed.com.br>
2022-09-30 19:55:36 -0300

    chore: adiciona pasta de coverage ao gitignore

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from './../protocols'
+import { HttpResponse } from '../protocols'
 import { ServerError } from '../errors'
 
 export const badRequest = (error: Error): HttpResponse => ({

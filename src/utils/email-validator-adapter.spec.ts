ee872c9212f949dc340d8676cd8c31e8a051ab1c
Elvio Batista <elvio.batista@pebmed.com.br>
2022-10-01 11:15:55 -0300

    refactor: cria factory method para helper na criação de sut de emailvalidator

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,17 +7,28 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   test('Retorna falso se o EmailValidator retornar falso', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
   test('Retorna true se o EmailValidator retornar true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('email@mail.com')
     expect(isValid).toBe(true)
+  })
+
+  test('Deve chamar o validator com o email correto', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('email@mail.com')
   })
 })

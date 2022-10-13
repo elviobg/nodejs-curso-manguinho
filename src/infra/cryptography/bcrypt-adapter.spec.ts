9de87e3faf6c9913995be39b39d69c4e1dd0355b
Elvio Batista <elvio.batista@pebmed.com.br>
2022-10-13 10:28:04 -0300

    test: garante que o adapter de criptografia é chamado com os parâmetros corretos

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt';
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
+
+describe('Bcrypt Adapter', () => {
+  test('Deve chamar o adapter com os valores corretos', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('value')
+    expect(hashSpy).toHaveBeenCalledWith('value', salt)
+  })
+})

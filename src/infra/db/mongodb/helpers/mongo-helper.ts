b55391c10736a5dc8fad290294a1a0fee533b785
Elvio Batista <elvio.batista@pebmed.com.br>
2022-10-25 21:28:55 -0300

    refactor: cria função genérica para mapear models

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,5 +16,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (model: any): any {
+    const { _id, ...modelWhitoutId } = model
+    return Object.assign({}, modelWhitoutId, { id: _id })
   }
 }
